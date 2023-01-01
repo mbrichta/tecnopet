cb3ac02f6dcafefffbd345d7face6b9b1a6e5365
Mathias Brichta <mathias.brichta@nexyo.io>
2023-01-01 18:56:17 +0100

    test

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "Info@yourdomain.com";
+$address = "mathias.brichta@hotmail.com";
 
 
 // Configuration option.
