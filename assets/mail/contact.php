2f8b4b3344d2f8d43af26d52c67273604268ccd0
Mathias Brichta <mathias.brichta@nexyo.io>
2023-01-02 22:55:32 +0100

    fixe email again

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -40,7 +40,7 @@ $comments = stripslashes($comments);
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "28mathias23@gmail";
+$address = "28mathias23@gmail.com";
 
 
 // Configuration option.
